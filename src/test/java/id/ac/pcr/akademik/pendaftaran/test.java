b3528cd1a319e04049ea666e2c069f5113f5c827
Memen Akbar <memen@pcr.ac.id>
2013-04-02 09:40:09 +0700

    test,java diedit

--- src/test/java/id/ac/pcr/akademik/pendaftaran/test.java
+++ src/test/java/id/ac/pcr/akademik/pendaftaran/test.java
@@ -9,5 +9,8 @@ package id.ac.pcr.akademik.pendaftaran;
  * @author LAB 316
  */
 public class test {
-    
+    public static void main(String args[]){
+        System.out.println("Hello World !");
+        System.out.println("Hello..");
+    }
 }
