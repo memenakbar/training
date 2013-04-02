2d28a27980d5e29d3c4e896bf02ff9243054620e
Memen Akbar <memen@pcr.ac.id>
2013-04-02 09:06:43 +0700

    test.java ditambahkan

---
+++ src/test/java/id/ac/pcr/akademik/pendaftaran/test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.pcr.akademik.pendaftaran;
+
+/**
+ *
+ * @author LAB 316
+ */
+public class test {
+    
+}
